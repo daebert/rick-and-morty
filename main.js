82a1263923a8f740632713ae159502be0ea5369a
daniel.ebert <daniel.ebert@web.de>
2021-05-04 15:31:16 +0200

    basic design

--- main.js
+++ main.js
@@ -1,6 +1,40 @@
+import { createCharacterElement } from "./components/character";
 import "./style.css";
+import { createElement } from "./utils/elements";
 
-document.querySelector("#app").innerHTML = `<h1>Hello Vite!</h1>
-<a href="https://vitejs.dev/guide/features.html" target="_blank">
-  Documentation
-</a>`;
+const headerElement = createElement("section", {
+  className: "header",
+  children: [
+    createElement("section", {
+      innerText: "The Rick and Morty API",
+      className: "header",
+    }),
+  ],
+});
+
+const mainElement = createElement("main", {
+  className: "main",
+  children: [
+    createElement("input", { placeholder: "Enter name" }),
+    createElement("section", {
+      className: "characters",
+      children: [
+        createCharacterElement({
+          imgSrc: "https://rickandmortyapi.com/api/character/avatar/2.jpeg",
+          name: "Rick Sanchez",
+          status: "alive",
+          species: "Human",
+          origin: {
+            name: "Earth",
+          },
+        }),
+      ],
+    }),
+    createElement("section", {
+      innerText: "Footer",
+      className: "footer",
+    }),
+  ],
+});
+
+document.querySelector("#app").append(headerElement, mainElement);
